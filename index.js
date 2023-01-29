a39a167b989c4ddf4095a8d0555c23290b3e6df8
alaa77alghamdi <98323604+alaa77alghamdi@users.noreply.github.com>
2023-01-29 18:40:04 +0300

    lab

---
+++ index.js
@@ -0,0 +1,21 @@
+"use strict";
+exports.__esModule = true;
+;
+var school = [
+    { id: 1, name: 'alaa', level: '5', isSenior: true },
+    { id: 2, name: 'sara', level: '2', isSenior: false },
+    { id: 3, name: 'noor', level: '4', isSenior: false }
+];
+function printStudent(arr) {
+    for (var _i = 0, arr_1 = arr; _i < arr_1.length; _i++) {
+        var student = arr_1[_i];
+        console.log("ID: ".concat(student.id, ", Name: ").concat(student.name, ", Level: ").concat(student.level, ", Senior: ").concat(student.isSenior, " "));
+    }
+}
+printStudent(school);
+;
+var tech = [
+    { id: 1, name: 'alaa', specialty: '5', courseNum: 7676 },
+    { id: 2, name: 'sara', specialty: '2', courseNum: 8686 },
+    { id: 3, name: 'noor', specialty: '4', courseNum: 9974 }
+];
---
+++ index.ts
@@ -0,0 +1,58 @@
+interface Student  {
+    id:number
+    name: string;
+    level: string;
+    isSenior: boolean;
+  };
+
+
+let school:Student[]= [
+{ id:1,name:'alaa', level:'5',isSenior:true},
+
+{ id:2,name:'sara', level:'2',isSenior:false},
+
+
+{ id:3,name:'noor', level:'4',isSenior:false}
+]
+
+
+function printStudent(arr:Student[]){
+    for (const student of arr) {
+      console.log(`ID: ${student.id}, Name: ${student.name}, Level: ${student.level}, Senior: ${student.isSenior} `);
+      
+    }
+    
+  }
+  printStudent(school)
+
+interface Teacher   {
+    id:number
+    name: string;
+    specialty: string;
+    courseNum: number;
+  };
+
+
+
+
+
+  let tech:Teacher[]= [
+    { id:1,name:'alaa', specialty:'5',courseNum:7676},
+    
+    { id:2,name:'sara', specialty:'2',courseNum:8686},
+    
+    
+    { id:3,name:'noor', specialty:'4',courseNum:9974}
+    ]
+    
+
+
+
+
+
+
+
+export{}
+
+
+ 
